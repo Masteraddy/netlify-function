8f3e658e3c4eb9d922f3e2a995d237000048f5fc
Green Sostein <greenfacilityltd@gmail.com>
2020-04-20 16:16:51 +0100

    Fixed Bug

--- lambda/api.js
+++ lambda/api.js
@@ -20,13 +20,39 @@ const Api = require('./model/Api');
 const router = express.Router();
 router.get('/', (req, res) => {
 	Api.find({})
-		.then((result) => res.status(200).json({ body: result }))
+		.then((result) => res.status(200).json({ data: result }))
 		.catch((err) => res.status(400).json({ error: err }));
 });
+
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+router.post('/', (req, res) => {
+	// res.json({ postBody: req.body })
+	var { type, name } = req.body;
+	var newpost = new Api({
+		name,
+		type,
+	});
+
+	newpost
+		.save()
+		.then((item) => res.json({ success: true, data: item }))
+		.catch((err) => res.status(404).json({ msg: `Not post due to ${err}` }));
+});
+
+router.delete('/:id', (req, res) => {
+	Api.findByIdAndDelete(req.params.id, (err, todo) => {
+		if (err) return res.status(500).send({ success: false, err });
+		return res.json({ success: true });
+	});
+});
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
 app.use('/.netlify/functions/api', router); // path must route to lambda
 
 module.exports = app;
